70190e69ece60e75d7985df2d0c26df8aa28211c
imgres <92025754+imgres2002@users.noreply.github.com>
2024-04-04 16:37:17 +0200

    Update cities_entry in submit function

--- gui.py
+++ gui.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from tkinter import ttk
-from src import read_data_from_txt_file, read_names_from_json_file, find_friends, find_user_by_name_and_surname, find_user_by_phone_number
+from src import *
 from tkinter.scrolledtext import ScrolledText
+
 
 class LegitCheckApp(tk.Tk):
     def __init__(self):
@@ -12,8 +13,9 @@ class LegitCheckApp(tk.Tk):
         self.selected_txt_file = ""
         self.selected_file = ""
         self.name_list = []
+        self.phone_list = []
 
-        # Ramka dla wszystkich elementów oprócz obszaru wyświetlania informacji z pliku JSON
+        # Ramka dla wszystkich elementów oprócz obszaru wyświetlania informacji
         main_frame = tk.Frame(self)
         main_frame.pack(expand=True, fill=tk.BOTH)
 
@@ -27,7 +29,8 @@ class LegitCheckApp(tk.Tk):
         self.info_label = tk.Label(left_frame, text="")
         self.info_label.pack(pady=5)
 
-        self.search_options = ttk.Combobox(left_frame, values=["Phone Number", "Name and Surname", "JSON File"])
+        self.search_options = ttk.Combobox(left_frame,
+                                           values=["Phone Number", "Name and Surname", "JSON File", "VCF File"])
         self.search_options.pack(pady=5)
         self.search_options.bind("<<ComboboxSelected>>", self.on_search_option_selected)
 
@@ -40,15 +43,15 @@ class LegitCheckApp(tk.Tk):
         self.submit_button = tk.Button(left_frame, text="Submit", command=self.submit)
         self.submit_button.pack(pady=5)
 
-        # Prawa część interfejsu użytkownika - obszar wyświetlania informacji z pliku JSON
+        # Prawa część interfejsu użytkownika - obszar wyświetlania informacji
         right_frame = tk.Frame(main_frame)
         right_frame.pack(side=tk.RIGHT, padx=10, pady=10)
 
-        self.json_info_label = tk.Label(right_frame, text="JSON File Information:")
-        self.json_info_label.pack()
+        self.file_info_label = tk.Label(right_frame, text="File Information:")
+        self.file_info_label.pack()
 
-        self.json_info_text = ScrolledText(right_frame, width=30, height=10)
-        self.json_info_text.pack()
+        self.file_info_text = ScrolledText(right_frame, width=30, height=10)
+        self.file_info_text.pack()
 
     def select_txt_file(self):
         self.selected_txt_file = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
@@ -60,20 +63,36 @@ class LegitCheckApp(tk.Tk):
             self.selected_file = filedialog.askopenfilename(filetypes=[("JSON files", "*.json")])
             self.entry_label.config(text="Enter cities (comma-separated) or leave empty:")
             self.display_json_info()
+        elif selection == "VCF File":
+            self.selected_file = filedialog.askopenfilename(filetypes=[("VCF files", "*.vcf")])
+            self.entry_label.config(text="Leave empty")
+            self.display_vcf_info()
         else:
-            self.entry_label.config(text=f"Enter {'Phone Number' if selection == 'Phone Number' else 'Name and Surname'}:")
-            self.json_info_text.delete(1.0, tk.END)
+            self.entry_label.config(
+                text=f"Enter {'Phone Number' if selection == 'Phone Number' else 'Name and Surname'}:")
+            self.file_info_text.delete(1.0, tk.END)
 
     def display_json_info(self):
         if self.selected_file:
-            json_info = read_names_from_json_file(self.selected_file)
-            self.json_info_text.delete(1.0, tk.END)
-            self.json_info_text.insert(tk.END, json_info)
+            json_info = read_data_from_json_file(self.selected_file)
+            self.file_info_text.delete(1.0, tk.END)
+            self.file_info_text.insert(tk.END, json_info)
+            self.adjust_results_width()
+
+    def display_vcf_info(self):
+        if self.selected_file:
+            vcf_info = read_data_from_vcf_file(self.selected_file)
+            self.file_info_text.delete(1.0, tk.END)
+            self.file_info_text.insert(tk.END, vcf_info)
+            self.adjust_results_width()
+
+    def adjust_results_width(self):
+        max_line_length = max(len(line) for line in self.file_info_text.get("1.0", "end").split("\n"))
+        self.file_info_text.configure(width=max_line_length)
 
     def submit(self):
         search_option = self.search_options.get()
-        cities_entry = self.entry_box.get()
-        cities = [city.strip() for city in cities_entry.split(",")] if cities_entry else None
+        cities_entry = self.entry_box.get().strip()
         if not self.selected_txt_file:
             messagebox.showerror("Error", "Please select a TXT file.")
             return
@@ -82,11 +101,21 @@ class LegitCheckApp(tk.Tk):
         elif search_option == "Name and Surname":
             result = find_user_by_name_and_surname(self.selected_txt_file, self.entry_box.get())
         elif search_option == "JSON File":
-            self.name_list = read_names_from_json_file(self.selected_file)
+            self.name_list = read_data_from_json_file(self.selected_file)
             if not self.name_list:
                 messagebox.showerror("Error", "No friends found in the JSON file.")
                 return
-            result = find_friends(self.selected_txt_file, self.name_list, cities)
+            if cities_entry:
+                cities = [city.strip() for city in cities_entry.split(",")]
+                result = find_friends_json(self.selected_txt_file, self.name_list, cities)
+            else:
+                result = find_friends_json(self.selected_txt_file, self.name_list)
+        elif search_option == "VCF File":
+            self.phone_list = read_data_from_vcf_file(self.selected_file)
+            if not self.phone_list:
+                messagebox.showerror("Error", "No friends found in the VCF file.")
+                return
+            result = find_friends_vcf(self.selected_txt_file, self.phone_list)
         else:
             messagebox.showerror("Error", "Invalid search option.")
             return
@@ -105,6 +134,11 @@ class LegitCheckApp(tk.Tk):
         for line in result:
             result_text.insert(tk.END, line + "\n")
         result_text.pack(expand=True, fill=tk.BOTH)
+        result_text.update_idletasks()
+
+        max_line_length = max(len(line) for line in result_text.get("1.0", "end").split("\n"))
+        result_text.configure(width=max_line_length)
+
 
 if __name__ == "__main__":
     app = LegitCheckApp()
